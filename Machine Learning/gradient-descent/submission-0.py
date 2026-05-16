cd769e596a7818e6a667075f5b7640d7726fe56f
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2026-05-16 07:11:41 -0500

    Remove Legacy Machine Learning/ folder

--- Machine Learning/gradient-descent/submission-0.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
-        for i in range(iterations):
-            init -= learning_rate * 2 * init
-        return round(init, 5)
